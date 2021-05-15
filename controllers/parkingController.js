313b8d7378d25d2e899f2c68eab82f3f74cd41ae
abdulrehman <arehmana377@gmail.com>
2021-05-15 21:44:53 +0500

    Update

--- controllers/parkingController.js
+++ controllers/parkingController.js
@@ -3,6 +3,7 @@ import Parking from '../modals/parkingModal.js';
 import asyncHandler from 'express-async-handler';
 import User from '../modals/userModal.js';
 import { Customer } from '../modals/parkingModal.js';
+import moment from 'moment';
 
 // @desc    Create Parking Area
 // @route   POST /api/parking/create
@@ -76,69 +77,123 @@ const getMyParking = asyncHandler(async (req, res) => {
 // @access  Private
 const createBooking = asyncHandler(async (req, res) => {
   const { parkingID, startdate, enddate, carnumber, slots } = req.body;
+  const pakStartDate = moment(startdate).format();
+  const pakEndDate = moment(enddate).format();
+
   const parking = await Parking.findById(parkingID);
   const user = await User.findById(req.user._id).select('-password');
+
   const customer = await Customer.find({
     customerID: req.user._id,
     isExpired: false,
     parkingID,
-    $or: [
-      {
-        $and: [
-          { startdate: { $gte: new Date(startdate) } },
-          { enddate: { $lte: new Date(startdate) } },
-        ],
-      },
-      {
-        $and: [
-          { startdate: { $gte: new Date(enddate) } },
-          { enddate: { $lte: new Date(enddate) } },
-        ],
-      },
-      {
-        $and: [
-          { startdate: { $lte: new Date(startdate) } },
-          { enddate: { $gte: new Date(enddate) } },
-        ],
-      },
-    ],
+    // $or: [
+    //   {
+    //     $and: [
+    //       { startdate: { $gte:  startdate } }, // 2  1
+    //       { enddate: { $lte: startdate } }, //  3    1
+    //     ],
+    //   },
+    //   {
+    //     $and: [
+    //       { startdate: { $gte: enddate } }, // 2   3
+    //       { enddate: { $lte: enddate } }, //    3  3
+    //     ],
+    //   },
+    //   {
+    //     $and: [
+    //       { startdate: { $lte: startdate } }, //  2   1
+    //       { enddate: { $gte: enddate } },  // 3  3
+    //     ],
+    //   },
+    // ],
   });
-  if (customer.length >= 3) {
-    console.log(customer.length);
-    res.status(400);
-    throw new Error('You alreday book 3 times in this Parking Area');
-  } else {
-    console.log(customer.length);
 
-    if (parking) {
-      const customer = await Customer.create({
-        customerID: req.user._id,
-        name: req.user.name,
-        startdate,
-        enddate,
-        carnumber,
-        slots,
-        parkingID,
-      });
-      try {
-        const sess = await mongoose.startSession();
-        await sess.startTransaction();
-        parking.customers.push(customer._id);
-        await parking.save();
-        user.parkings.push(customer._id);
-        await user.save();
-        sess.commitTransaction();
-      } catch (error) {
-        console.log(error);
-        res.status(400);
-        throw new Error('Create  Parking Failed');
+  if (parking) {
+    var check = 0;
+    const count = customer.forEach((obj) => {
+      if (
+        (moment(obj.startdate).format() <= moment(startdate).format() &&
+          moment(obj.enddate).format() > moment(startdate).format()) ||
+        (moment(obj.startdate).format() < moment(enddate).format() &&
+          moment(obj.enddate).format() >= moment(enddate).format()) ||
+        (moment(obj.startdate).format() <= moment(startdate).format() &&
+          moment(obj.enddate).format() >= moment(enddate).format())
+      ) {
+        check++;
+        // return obj;
+        console.log(obj);
       }
-      res.json({ success: true, data: parking });
-    } else {
+    });
+
+    //   3        >=       1
+    //    6       <=       1   false
+    //   3        >=       5   False
+    //    6       <=       5
+    //   3        <=       1  False
+    //    6       >=       5
+
+    console.log(count);
+    if (check >= 3) {
       res.status(400);
-      throw new Error('Not Parking Found');
+      throw new Error('You alreday book 3 times in this Parking Area');
+    } else {
+      //create Customer
+      // const customer = await Customer.create({
+      //   customerID: req.user._id,
+      //   name: req.user.name,
+      //   startdate,
+      //   enddate,
+      //   carnumber,
+      //   slots,
+      //   parkingID,
+      // });
+      res.json({
+        success: `You are right ${check} is less than 3`,
+        data: count,
+      });
     }
+  } else {
+    res.status(400);
+    throw new Error('Not Parking Found');
   }
+
+  // if (customer.length >= 3) {
+  //   console.log(customer.length);
+  //   res.status(400);
+  //   throw new Error('You alreday book 3 times in this Parking Area');
+  // } else {
+  //   console.log(customer.length);
+
+  //   if (parking) {
+  //     const customer = await Customer.create({
+  //       customerID: req.user._id,
+  //       name: req.user.name,
+  //       startdate,
+  //       enddate,
+  //       carnumber,
+  //       slots,
+  //       parkingID,
+  //     });
+  //     try {
+  //       const sess = await mongoose.startSession();
+  //       await sess.startTransaction();
+  //       parking.customers.push(customer._id);
+  //       await parking.save();
+  //       user.parkings.push(customer._id);
+  //       await user.save();
+  //       sess.commitTransaction();
+  //     } catch (error) {
+  //       console.log(error);
+  //       res.status(400);
+  //       throw new Error('Create  Parking Failed');
+  //     }
+  //     res.json({ success: true, data: parking });
+  //   } else {
+  //     res.status(400);
+  //     throw new Error('Not Parking Found');
+  //   }
+  // }
 });
 
 // @desc    Accept Booking in Parking Area
